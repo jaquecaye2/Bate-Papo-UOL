46b9097826c19d95a99d4e48fd13b417e19265df
jaquecaye <jaquelinecaye@gmail.com>
2022-04-17 16:16:52 -0300

    Bônus: Participantes ativos

--- script.js
+++ script.js
@@ -85,14 +85,104 @@ setInterval(manterConexao, 4500)
 
 setInterval(buscarMensagens, 3000)
 
+function menuLateralAparecer(){
+    document.querySelector(".menu-lateral").classList.remove("escondido")
+}
+
+function menuLateralDesaparecer(){
+    document.querySelector(".menu-lateral").classList.add("escondido")
+}
+
+let contatoSelecionado
+let nomeContatoSelecionado
+
+function contatoSelelecionado(elemento){
+    contatoSelecionado = document.querySelector(".contato-selecionado")
+
+    if (contatoSelecionado !== null){
+        contatoSelecionado.classList.remove('contato-selecionado')
+    }
+    elemento.querySelector(".checkmark").classList.add("contato-selecionado")
+
+    nomeContatoSelecionado = elemento.querySelector("p").innerHTML
+
+    if (nomeContatoSelecionado !== "Todos"){
+        document.querySelector(".visibilidade-selecionada").classList.remove("visibilidade-selecionada")
+        document.querySelector(".reservado .checkmark").classList.add("visibilidade-selecionada")
+
+        document.querySelector("footer input").placeholder = `Escreva aqui... \n Enviando para ${nomeContatoSelecionado} (reservadamente)`
+    }
+    if (nomeContatoSelecionado === "Todos"){
+        document.querySelector(".visibilidade-selecionada").classList.remove("visibilidade-selecionada")
+        document.querySelector(".publico .checkmark").classList.add("visibilidade-selecionada")
+
+        document.querySelector("footer input").placeholder = "Escreva aqui..."
+    }
+}
+
+let participantes = []
+
+function atualizarParticipantes(){
+    let ulParticipantes = document.querySelector(".contatos")
+
+    ulParticipantes.innerHTML = ""
+
+    ulParticipantes.innerHTML = `
+        <div class="contato" onclick="contatoSelelecionado(this)">
+            <div>
+                <ion-icon name="people" class="icone-contato"></ion-icon>
+                <p>Todos</p>
+            </div>
+            <ion-icon name="checkmark" class="checkmark contato-selecionado"></ion-icon>
+        </div>`
+
+    for (let i = 0; i < participantes.length; i++){
+        if (participantes[i].name !== nome){
+            ulParticipantes.innerHTML += `
+            <div class="contato" onclick="contatoSelelecionado(this)">
+                <div>
+                    <ion-icon name="person-circle" class="icone-contato"></ion-icon>
+                    <p>${participantes[i].name}</p>
+                </div>
+                <ion-icon name="checkmark" class="checkmark"></ion-icon>
+            </div>`
+        }
+    }
+}
+
+function listaParticipantes(){
+    let promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+
+    promise.then(function (response){
+        participantes = response.data
+        atualizarParticipantes()
+    })
+
+    document.querySelector(".visibilidade-selecionada").classList.remove("visibilidade-selecionada")
+    document.querySelector(".publico .checkmark").classList.add("visibilidade-selecionada")
+}
+
+setInterval(listaParticipantes, 10000)
+
 function enviarMensagem(){
     let mensagem = document.querySelector("footer input").value
 
-    let objMensagem = {
-        from: nome,
-        to: "Todos",
-        text: mensagem,
-        type: "message"
+    let objMensagem
+
+    if (nomeContatoSelecionado === "Todos"){
+        objMensagem = {
+            from: nome,
+            to: "Todos",
+            text: mensagem,
+            type: "message"
+        }
+    } else {
+        objMensagem = {
+            from: nome,
+            to: nomeContatoSelecionado,
+            text: mensagem,
+            type: "private_message"
+        }
     }
 
     let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", objMensagem)
@@ -104,4 +194,5 @@ function enviarMensagem(){
         window.location.reload()
     })
 }
+
 
